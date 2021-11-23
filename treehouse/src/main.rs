8ad01b29bd752a6bee579fc9578b63b3428ad26f
euvue <eeuv@tuta.io>
2021-11-22 18:39:35 -0700

    add println

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -3,8 +3,9 @@ use std::io::stdin;
 fn main() {
     println!("Hello, what's your name?");
     let mut your_name = String::new();
-    
+
     stdin()
-	.read_line(&mut your_name)
-	.expect("Failed to read line");
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    println!("Hello, {}", your_name)
 }
